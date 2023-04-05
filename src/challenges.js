64fe01aed34b21f96bff026fe3db7ea58b411781
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-05 17:35:52 -0300

    Iniciando a seção 5

--- src/challenges.js
+++ src/challenges.js
@@ -13,7 +13,22 @@ const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 // Desafio 4 - Crie a função footballPoints
 const footballPoints = (wins, ties) => wins * 3 + ties * 1;
 // Desafio 5 - Crie a função highestCount
-
+const highestCount = (array) => {
+  let maiorNumero = array[0];
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > maiorNumero) {
+      maiorNumero = array[index];
+    }
+  }
+  let repeticao = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === maiorNumero) {
+      repeticao += 1;
+    }
+  }
+  return repeticao;
+};
+console.log(highestCount([3, 3, 9, 1, 2, 3, 9, 5, 7]));
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
 // Desafio 7 - Crie a função catAndMouse
