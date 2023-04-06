84faab113c2ad1b2d19319bb02f8150c3e127ece
Willian Gonçalves <willian.p.g@outlook.com>
2023-04-06 15:46:35 -0300

    Nova seção com codigo muito extenso

--- src/challenges.js
+++ src/challenges.js
@@ -46,7 +46,7 @@ const catAndMouse = (mouse, cat1, cat2) => {
   }
   return 'os gatos trombam e o rato foge';
 };
-console.log(catAndMouse(5, 3, 7));
+console.log(catAndMouse(1, 3, 7));
 // Desafio 8 - Crie a função fizzBuzz
 const fizzBuzz = (array) => {
   let array2 = [];
@@ -64,7 +64,44 @@ const fizzBuzz = (array) => {
 };
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9 - Crie a função encode e a função decode
-
+const encode = (vogal) => {
+  let frase = [];
+  for (let index = 0; index < vogal.length; index += 1) {
+    if (vogal[index] === 'a') {
+      frase.push('1');
+    } else if (vogal[index] === 'e') {
+      frase.push('2');
+    } else if (vogal[index] === 'i') {
+      frase.push('3');
+    } else if (vogal[index] === 'o') {
+      frase.push('4');
+    } else if (vogal[index] === 'u') {
+      frase.push('5');
+    } else {
+      frase.push(vogal[index]);
+    }
+  } return frase.join('');
+};
+const decode = (vogal) => {
+  let frase = [];
+  for (let index = 0; index < vogal.length; index += 1) {
+    if (vogal[index] === '1') {
+      frase.push('a');
+    } else if (vogal[index] === '2') {
+      frase.push('e');
+    } else if (vogal[index] === '3') {
+      frase.push('i');
+    } else if (vogal[index] === '4') {
+      frase.push('o');
+    } else if (vogal[index] === '5') {
+      frase.push('u');
+    } else {
+      frase.push(vogal[index]);
+    }
+  } return frase.join('');
+};
+console.log(encode('Ola tryber bom dia'));
+console.log(decode('Ol1 tryb2r b4m d31'));
 // Desafio 10 - Crie a função techList
 
 // Não modifique essas linhas
